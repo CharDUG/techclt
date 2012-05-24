b9ba69f10566330ff8c8a539291f9bc62585f4dc
Mark Shropshire <marks@knowclassic.com>
2012-05-23 20:48:53 -0400

    Removed old themes

--- www/sites/all/themes/adaptivetheme/adaptivetheme/templates/aggregator-summary-items.tpl.php
+++
@@ -1,5 +0,0 @@
-<section id="feed-<?php print drupal_html_class($title); ?>" class="feed">
-  <h2 class="summary-title"><?php print $title; ?></h2>
-  <?php print $summary_list; ?>
-  <p class="read-more"><a href="<?php print $source_url; ?>"><?php print t('More'); ?></a></p>
-</section>
--- www/sites/all/themes/adaptivetheme/adaptivetheme/templates/region--content-aside.tpl.php
+++
@@ -1,5 +0,0 @@
-<?php if (!empty($content)): ?>
-  <aside class="<?php print $classes; ?>">
-    <?php print $content; ?>
-  </aside>
-<?php endif; ?>
